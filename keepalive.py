7cd25b4ed8a5afe7905eb5c41025eb89a100ce51
Vista <140166594+Vista000@users.noreply.github.com>
2025-06-15 18:09:45 +0330

    Update keepalive.py

--- keepalive.py
+++ keepalive.py
@@ -4,17 +4,13 @@ import os
 
 app = Flask('')
 
-
 @app.route('/')
 def home():
     return "I am alive!"
 
-
 def run():
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
-
-
 
 def keep_alive():
     t = Thread(target=run)
